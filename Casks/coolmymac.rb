e9f406764f805dd795dee6643d5e0e1e9dc10fb7
Tucker Willenborg <tuckerwillenborg@gmail.com>
2026-05-25 23:02:04 -0400

    fix: Update SHA256 for CLI error handling bugfix

--- Casks/coolmymac.rb
+++ Casks/coolmymac.rb
@@ -1,6 +1,6 @@
 cask "coolmymac" do
   version "1.0.5"
-  sha256 "5c04f17c10c3f30778923de130270b535240d1464b1e468df7b2fb5b3f820eca"
+  sha256 "a80cd6d3f83822a68b311d27408c6b6efbb51b48de342d12d08d65fe8b2ce293"
 
   url "https://github.com/ecc521/CoolMyMac/releases/download/v#{version}/CoolMyMac.dmg"
   name "CoolMyMac"
